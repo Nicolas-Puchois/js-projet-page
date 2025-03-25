5b07a3d7b17d78ac6f6bc8b4cbbdddd5abcca1b6
Nicolas-Puchois <nicolas.puchois@laposte.net>
2025-03-25 15:16:44 +0100

    ajout dark-mode

--- frontend/assets/js/header/header.js
+++ frontend/assets/js/header/header.js
@@ -7,42 +7,47 @@ const toggleNavMobile = () => {
   menuIcon(navMobile);
 };
 
+// const closeMenu = () => {
+//   // INFO : cache la navigation telephone lors du clic sur la page ou click sur un lien ou lors du scroll
+
+//   // balise <main>
+//   let closeMenuMain = document.querySelector("main");
+
+//   // balise <div id="nav"> dans la <nav>
+//   let navMobile = document.querySelector("nav > #nav");
+
+//   // Toutes les balises <a> dans les <li> dans les <ul> dans la <div id="nav"
+//   let links = document.querySelectorAll("#nav > ul > li > a");
+
+//   // ferme le menu quand touche sur le main
+//   closeMenuMain.addEventListener("click", () => {
+//     navMobile.classList.remove("mobile");
+//     menuIcon(navMobile);
+//   });
+
+//   // ferme le menu lors de l'appuie d'un lien
+//   links.forEach((link) => {
+//     link.addEventListener("click", () => {
+//       navMobile.classList.remove("mobile");
+//       menuIcon(navMobile);
+//     });
+//   });
+
+//   // ferme le menu lors du scroll
+//   window.addEventListener("scroll", () => {
+//     navMobile.classList.remove("mobile");
+//     menuIcon(navMobile);
+//   });
+
+//   // window.addEventListener("resize", () => {
+//   //   navMobile.classList.remove("mobile");
+//   //   menuIcon(navMobile);
+//   // });
+// };
+
 const closeMenu = () => {
-  // INFO : cache la navigation telephone lors de l'appuie sur la page ou click sur un lien ou lors du scroll
-
-  // balise <main>
-  let closeMenuMain = document.querySelector("main");
-
-  // balise <div id="nav"> dans la <nav>
   let navMobile = document.querySelector("nav > #nav");
-
-  // Toutes les balises <a> dans les <li> dans les <ul> dans la <div id="nav"
-  let links = document.querySelectorAll("#nav > ul > li > a");
-
-  // ferme le menu quand touche sur le main
-  closeMenuMain.addEventListener("click", () => {
-    navMobile.classList.remove("mobile");
-    menuIcon(navMobile);
-  });
-
-  // ferme le menu lors de l'appuie d'un lien
-  links.forEach((link) => {
-    link.addEventListener("click", () => {
-      navMobile.classList.remove("mobile");
-      menuIcon(navMobile);
-    });
-  });
-
-  // ferme le menu lors de l'appuie d'un lien
-  window.addEventListener("scroll", () => {
-    navMobile.classList.remove("mobile");
-    menuIcon(navMobile);
-  });
-
-  // window.addEventListener("resize", () => {
-  //   navMobile.classList.remove("mobile");
-  //   menuIcon(navMobile);
-  // });
+  navMobile.classList.remove("mobile");
 };
 
 const menuIcon = (navMobile) => {
@@ -56,10 +61,25 @@ const menuIcon = (navMobile) => {
   );
 };
 
-window.addEventListener("DOMContentLoaded", () => {
-  if (window.matchMedia("(max-width:768px").matches) {
+function initMobileMenu() {
+  if (window.matchMedia("(max-width:600px").matches) {
     let toggleNav = document.querySelector("#toggle-nav");
     toggleNav.addEventListener("click", toggleNavMobile);
+    toggleNav.computedStyleMap.cursor = "pointer";
+    let main = document.querySelector("main");
+    main.addEventListener("click", closeMenu);
+    let navLinks = document.querySelectorAll("nav > #nav > ul > li >a");
+    navLinks.forEach((navLink) => {
+      navLink.addEventListener("click", closeMenu);
+    });
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener("resize", initMobileMenu);
+  if (window.matchMedia("(max-width:600px").matches) {
+    let toggleNav = document.querySelector("#toggle-nav");
+    // toggleNav.addEventListener("click", toggleNavMobile);
     toggleNav.addEventListener("click", closeMenu);
   }
 });
